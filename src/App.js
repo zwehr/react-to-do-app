515d6fe70c0c9db15473c8e47e0d54ca0437cc8a
zwehr <zwehrmeister@gmail.com>
2017-06-05 15:06:38 -0400

    add to-dos state to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import AddProject from "./components/AddProject";
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toDos: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({toDos: [
+      "An example task. Click the 'X' to delete.",
+      "Example 2"
+    ]
+  });
+  }
+
   render() {
+    console.log(this.state.toDos);
     return (
       <div className="App">
         <AddProject />
--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -6,7 +6,7 @@ class AddProject extends Component {
       <div>
         <h2>What do you need to do today?</h2>
         <form>
-          <input type="text" ref="task" placeholder="Enter a task here" />
+          <input type="text" ref="toDo" placeholder="Enter a task here" />
           <input type="submit" value="Add to list" />
         </form>
       </div>
